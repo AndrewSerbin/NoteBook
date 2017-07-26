1bb751af30012b99b0807f9391ca4782b5543594
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 17:09:42 +0300

    Added model structure.

---
+++ src/main/java/ua/com/andrewserbin/model/Model.java
@@ -0,0 +1,7 @@
+package ua.com.andrewserbin.model;
+
+/**
+ * Created by layer on 26.07.2017.
+ */
+public class Model {
+}
---
+++ src/main/java/ua/com/andrewserbin/model/entity/NoteBook.java
@@ -0,0 +1,7 @@
+package ua.com.andrewserbin.model.entity;
+
+/**
+ * Created by layer on 26.07.2017.
+ */
+public class NoteBook {
+}
