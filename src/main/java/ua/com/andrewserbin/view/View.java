d662344e5aa326a3ee3010b695cca5763491f908
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 19:02:13 +0300

    Added view realization.

--- src/main/java/ua/com/andrewserbin/view/View.java
+++ src/main/java/ua/com/andrewserbin/view/View.java
@@ -4,4 +4,27 @@ package ua.com.andrewserbin.view;
  * Created by layer on 26.07.2017.
  */
 public class View {
+
+    /**
+     * Prints message in console.
+     * @param message
+     */
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+
+    /**
+     * Concatenates messages and prints them in console.
+     * @param messages
+     */
+    public void printMessage(String... messages) {
+        StringBuilder builder = new StringBuilder();
+
+        for (String message :
+                messages) {
+            builder.append(message);
+        }
+
+        System.out.println(builder);
+    }
 }
--- src/main/java/ua/com/andrewserbin/view/ViewConstants.java
+++ src/main/java/ua/com/andrewserbin/view/ViewConstants.java
@@ -4,4 +4,8 @@ package ua.com.andrewserbin.view;
  * Created by layer on 26.07.2017.
  */
 public interface ViewConstants {
+
+    String INPUT_NAME = "Input name: ";
+
+    String WRONG_INPUT = "Wrong input! Repeat pls. ";
 }
