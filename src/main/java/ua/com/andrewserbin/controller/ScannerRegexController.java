8f3a389682f1622cff6c5c8f7cfbc9dacce0bca4
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 18:05:12 +0300

    Added regex test.

--- src/main/java/ua/com/andrewserbin/controller/ScannerRegexController.java
+++ src/main/java/ua/com/andrewserbin/controller/ScannerRegexController.java
@@ -4,4 +4,8 @@ package ua.com.andrewserbin.controller;
  * Created by layer on 26.07.2017.
  */
 public class ScannerRegexController {
+
+    public static String inputStringInAccordanceWithRegex(String outputMessage, String regex) {
+        return "";
+    }
 }
