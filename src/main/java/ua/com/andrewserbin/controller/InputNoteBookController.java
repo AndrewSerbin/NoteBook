57e9d5d11808ff38afecff581de111394f938c39
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 18:54:26 +0300

    Added records fields to controller.

--- src/main/java/ua/com/andrewserbin/controller/InputNoteBookController.java
+++ src/main/java/ua/com/andrewserbin/controller/InputNoteBookController.java
@@ -1,7 +1,32 @@
 package ua.com.andrewserbin.controller;
 
+import ua.com.andrewserbin.model.entity.Address;
+import ua.com.andrewserbin.model.entity.FullName;
+import ua.com.andrewserbin.model.entity.Group;
+import ua.com.andrewserbin.model.entity.Telephone;
+
 /**
  * Created by layer on 26.07.2017.
  */
 public class InputNoteBookController {
+
+    private FullName fullName;
+
+    private String nick;
+
+    private String description;
+
+    private Group group;
+
+    private Telephone telephone;
+
+    private String email;
+
+    private String skype;
+
+    private Address address;
+
+    public void inputRecord() {
+        
+    }
 }
