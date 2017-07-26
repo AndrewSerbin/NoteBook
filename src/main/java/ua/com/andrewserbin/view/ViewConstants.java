23e24725aa04865a35b8df1493ac293a5f69d7b3
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 23:42:44 +0300

    Added view constants.

--- src/main/java/ua/com/andrewserbin/view/ViewConstants.java
+++ src/main/java/ua/com/andrewserbin/view/ViewConstants.java
@@ -6,6 +6,19 @@ package ua.com.andrewserbin.view;
 public interface ViewConstants {
 
     String INPUT_NAME = "Input name: ";
-
+    String INPUT_SURNAME = "Input surname: ";
+    String INPUT_PATRONYMIC = "Input patronymic: ";
+    String INPUT_NICK = "Input nick: ";
+    String INPUT_DESCRIPTION = "Input description: ";
+    String INPUT_MAIN_TELEPHONE = "Input main telephone: ";
+    String INPUT_SPARE_TELEPHONE = "Input spare telephone: ";
+    String INPUT_EMAIL = "Input email: ";
+    String INPUT_SKYPE = "Input skype: ";
+    String INPUT_INDEX = "Input index: ";
+    String INPUT_CITY = "Input city: ";
+    String INPUT_STREET = "Input street: ";
+    String INPUT_HOUSE_NUMBER = "Input house number: ";
+    String INPUT_APARTMENT_NUMBER = "Input apartment number: ";
     String WRONG_INPUT = "Wrong input! Repeat pls. ";
+
 }
