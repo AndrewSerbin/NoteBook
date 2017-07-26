8ff71f5453098658828679d8462179ae6c13a862
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 18:04:57 +0300

    Added model and view to controller.

--- src/main/java/ua/com/andrewserbin/controller/Controller.java
+++ src/main/java/ua/com/andrewserbin/controller/Controller.java
@@ -1,7 +1,22 @@
 package ua.com.andrewserbin.controller;
 
+import ua.com.andrewserbin.model.Model;
+import ua.com.andrewserbin.view.View;
+
 /**
  * Created by layer on 26.07.2017.
  */
 public class Controller {
+
+    Model model;
+    View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser() {
+
+    }
 }
