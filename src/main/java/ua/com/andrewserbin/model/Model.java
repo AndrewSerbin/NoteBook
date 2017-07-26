06e67b47f40ecd768f8c2049fd8574ec6784798b
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 23:45:02 +0300

    Added note book to model.

--- src/main/java/ua/com/andrewserbin/model/Model.java
+++ src/main/java/ua/com/andrewserbin/model/Model.java
@@ -1,7 +1,20 @@
 package ua.com.andrewserbin.model;
 
+import ua.com.andrewserbin.model.entity.NoteBook;
+import ua.com.andrewserbin.model.entity.Record;
+
 /**
  * Created by layer on 26.07.2017.
  */
 public class Model {
+
+    private NoteBook noteBook;
+
+    public Model() {
+        noteBook = new NoteBook();
+    }
+
+    public void addRecord(Record record) {
+        noteBook.add(record);
+    }
 }
