bf3b494da7f5c3a29fe54bde29558685e58fce6b
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 23:44:07 +0300

    Added regex controller realization.

--- src/main/java/ua/com/andrewserbin/controller/ScannerRegexController.java
+++ src/main/java/ua/com/andrewserbin/controller/ScannerRegexController.java
@@ -1,11 +1,30 @@
 package ua.com.andrewserbin.controller;
 
+import ua.com.andrewserbin.view.View;
+import ua.com.andrewserbin.view.ViewConstants;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  * Created by layer on 26.07.2017.
  */
 public class ScannerRegexController {
 
-    public static String inputStringInAccordanceWithRegex(String outputMessage, String regex) {
-        return "";
+    View view;
+
+    public ScannerRegexController(View view) {
+        this.view = view;
+    }
+
+    public String inputStringInAccordanceWithRegex(Scanner sc, String outputMessage, Pattern pattern) {
+        view.printMessage(outputMessage);
+
+        while (!sc.hasNext(pattern)) {
+            view.printMessage(ViewConstants.WRONG_INPUT + outputMessage);
+
+            sc.next();
+        }
+
+        return sc.next();
     }
 }
