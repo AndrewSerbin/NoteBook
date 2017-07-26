bd742116051a94e402a8f42f4d272264331346b8
Andrew Serbin <andrewserbin01@gmail.com>
2017-07-26 23:45:31 +0300

    Added controller initialization.

--- src/main/java/ua/com/andrewserbin/Main.java
+++ src/main/java/ua/com/andrewserbin/Main.java
@@ -1,8 +1,16 @@
 package ua.com.andrewserbin;
 
+import ua.com.andrewserbin.controller.Controller;
+import ua.com.andrewserbin.model.Model;
+import ua.com.andrewserbin.view.View;
+
 public class Main {
 
     public static void main( String[] args ) {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
 
+        controller.processUser();
     }
 }
